ebc93b279f933f27ec5dab04f30d58bbee632b58
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-05-11 22:28:49 +0530

    authentication half done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from 'react';
-
-import './App.css';
+import React, { useCallback, useState } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Users from "./admin/pages/admins";
+import MainNavigation from "./common/components/navigation/main-navigation";
+import Auth from "./admin/pages/authentication";
+import { AuthContext } from "./common/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uid) => {
+    setIsLoggedIn(true);
+    setUserId(uid);
+    
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+  }, []); //we have use callback here because we do not need to recreate(rerender) this element to the unwanted changes of the states and to prevent from infinite loops.
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users></Users>
+        </Route>
+        
+        
+        
+        <Redirect to="/"></Redirect>
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users></Users>
+        </Route>
+        
+        <Route path="/auth">
+          <Auth></Auth>
+        </Route>
+        <Redirect to="/auth"></Redirect>
+      </Switch>
+    );
+  }
   return (
-    <div className="App">
-      <h1>hello react</h1>
-    </div>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn,userId:userId, login: login, logout: logout }}
+    >
+      <Router>
+        <Route path="/">
+          <MainNavigation></MainNavigation>
+        </Route>
+        <main>
+          {routes}
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/admin/pages/admins.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useHttpClient } from "../../common/custom-hooks/http-hook";
+import UserList from "../components/user-list/user-list";
+import ErrorModal from "../../common/components/UIElements/model/error-model";
+import LoadingSpinner from "../../common/components/UIElements/loading-spinner/loading-spinner";
+
+const Users = () => {
+  const {isLoading,error,sendRequest,errorPopupCloser}=useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {// we have use useEffect hook to stop the infinite loop. otherwise fetch will rerender to all the changes. 
+    const fetchUsers = async () => {// this method is only to use the async code. we can't use async directly on the useEffect hook. useEffect is not good for promisses.
+      
+      try {
+        const responseData = await sendRequest("http://localhost:9000/api/admin/");
+        
+        
+        setLoadedUsers(responseData.admins);//this is the key of the response - view the backend code for more.
+      } catch (err) {
+        
+      }
+      
+    };
+    fetchUsers();
+  }, [sendRequest]);
+  
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorPopupCloser}></ErrorModal>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner></LoadingSpinner>
+        </div>
+      )}
+      {loadedUsers && !isLoading && <UserList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
