5fbb42978bf2da664db1c12050d0b1e5a060d645
Dasun <97machan@gmail.com>
2020-05-13 22:26:43 +0530

    Cart icon created, redux-essential folders created

--- src/App.js
+++ src/App.js
@@ -1,79 +1,80 @@
 import React, { useCallback, useState } from 'react';
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import Users from "./admin/pages/admins";
-import Categories from "./category/pages/categories";
-import MainNavigation from "./Common/components/navigation/main-navigation";
-import Auth from "./admin/pages/authentication";
-import { AuthContext } from "./Common/context/auth-context";
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Users from './admin/pages/admins';
+import Categories from './category/pages/categories';
+import ProductsByCat from './components/products/ProductsByCat';
+import MainNavigation from './Common/components/navigation/main-navigation';
+import Auth from './admin/pages/authentication';
+import { AuthContext } from './Common/context/auth-context';
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userId, setUserId] = useState(false);
+function App () {
+	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+	const [ userId, setUserId ] = useState(false);
 
-  const login = useCallback((uid) => {
-    setIsLoggedIn(true);
-    setUserId(uid);
-  }, []);
-  const logout = useCallback(() => {
-    setIsLoggedIn(false);
-    setUserId(null);
-  }, []); //we have use callback here because we do not need to recreate(rerender) this element to the unwanted changes of the states and to prevent from infinite loops.
+	const login = useCallback((uid) => {
+		setIsLoggedIn(true);
+		setUserId(uid);
+	}, []);
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+		setUserId(null);
+	}, []); //we have use callback here because we do not need to recreate(rerender) this element to the unwanted changes of the states and to prevent from infinite loops.
 
-  let routes;
-  if (isLoggedIn) {
-    routes = (
-      <Switch>
-        <Route path="/" exact>
-          <Categories></Categories>
-        </Route>
-        <Route path="/users" exact>
-          <Users></Users>
-        </Route>
-        
+	let routes;
+	if (isLoggedIn) {
+		routes = (
+			<Switch>
+				<Route path='/' exact>
+					<Categories />
+				</Route>
+				<Route path='/users' exact>
+					<Users />
+				</Route>
+				<Route exact path='/cat/:id'>
+					<ProductsByCat />
+				</Route>
 
-        <Redirect to="/"></Redirect>
-      </Switch>
-    );
-  } else {
-    routes = (
-      <Switch>
-        <Route path="/users" exact>
-          <Users></Users>
-        </Route>
-        <Route path="/" exact>
-          <Categories></Categories>
-        </Route>
+				<Redirect to='/' />
+			</Switch>
+		);
+	} else {
+		routes = (
+			<Switch>
+				<Route path='/users' exact>
+					<Users />
+				</Route>
+				<Route path='/' exact>
+					<Categories />
+				</Route>
+				<Route exact path='/cat/:id'>
+					<ProductsByCat />
+				</Route>
+				<Route path='/auth'>
+					<Auth />
+				</Route>
+				<Redirect to='/auth' />
+			</Switch>
+		);
+	}
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				userId: userId,
+				login: login,
+				logout: logout
+			}}
+		>
+			<Router>
+				<Route path='/'>
+					<MainNavigation />
+				</Route>
 
-        <Route path="/auth">
-          <Auth></Auth>
-        </Route>
-        <Redirect to="/auth"></Redirect>
-      </Switch>
-    );
-  }
-  return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        userId: userId,
-        login: login,
-        logout: logout,
-      }}
-    >
-      <Router>
-        <Route path="/">
-          <MainNavigation></MainNavigation>
-        </Route>
-        <main>{routes}</main>
-      </Router>
-    </AuthContext.Provider>
-  );
+				<main>{routes}</main>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 export default App;
--- src/category/components/category-item/category-item.jsx
+++ src/category/components/category-item/category-item.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./category-item.scss"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './category-item.scss';
 
-const CategoryItem = props => {
-  return (
-    <Link className={`menu-item`} 
-    to={`/${props.id}/items`}>
-      <div className='background-img'
-          style={{
-              backgroundImage: `url(${props.image})`
-          }} />
-      <div className='content'>
-          <h1 className='title'>{props.name}</h1>
-          <span className='subtitle'>SHOP NOW</span>
-      </div>
-    </Link>
-  );
+const CategoryItem = (props) => {
+	console.log(props);
+	return (
+		<Link className={`menu-item`} to={`/cat/${props.id}`} key={props.id}>
+			<div
+				className='background-img'
+				style={{
+					backgroundImage: `url(${props.image})`
+				}}
+			/>
+			<div className='content'>
+				<h1 className='title'>{props.name}</h1>
+				<span className='subtitle'>SHOP NOW</span>
+			</div>
+		</Link>
+	);
 };
 
 export default CategoryItem;
