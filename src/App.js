ccdacc20b9aba04a7d5a9384b230e5ad34bf65c5
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-05-16 23:58:56 +0530

    Storing the token in the browser storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from "react";
 
 import "./App.css";
 import {
@@ -9,35 +9,83 @@ import {
 } from "react-router-dom";
 import Users from "./admin/pages/admins";
 import Categories from "./category/pages/categories";
-import ProductsByCat from './components/products/ProductsByCat';
+import ProductsByCat from "./components/products/ProductsByCat";
 import MainNavigation from "./Common/components/navigation/main-navigation";
 import NewCategory from "./category/pages/new-category";
 import NewProduct from "./components/products/new-product";
 import Auth from "./admin/pages/authentication";
 import { AuthContext } from "./Common/context/auth-context";
 
+function App() {
+  const [token, setToken] = useState(false);
+  const [userId, setUserId] = useState(false);
+  const [tokenExpAuto, setTokenExpAuto] = useState();
 
-function App () {
-	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
-	const [ userId, setUserId ] = useState(false);
+  let logoutTime;
 
-	const login = useCallback((uid) => {
-		setIsLoggedIn(true);
-		setUserId(uid);
-	}, []);
-	const logout = useCallback(() => {
-		setIsLoggedIn(false);
-		setUserId(null);
-	}, []); //we have use callback here because we do not need to recreate(rerender) this element to the unwanted changes of the states and to prevent from infinite loops.
+  const login = useCallback((uid, token, tokenExpDate) => {
+    setToken(token);
+    setUserId(uid);
+    let tokenExp;
+    if (!tokenExpDate) {
+      tokenExp = new Date(new Date().getTime() + 4000); //Creating a new Expiration date => current time+1hr
+      setTokenExpAuto(tokenExp);
+    } else {
+      tokenExp = tokenExpDate; //Assigning the old date
+      setTokenExpAuto(tokenExp);
+    }
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        tokenExp: tokenExp.toISOString(),//setting expiration time for the token we have storing in the local storage
+      })
+    );
+  }, []);
+  const logout = useCallback(() => {
+    setToken(false);
+    setUserId(null);
+    setTokenExpAuto(null);
+    localStorage.removeItem("userData");
+  }, []); //we have use callback here because we do not need to recreate(rerender) this element to the unwanted changes of the states and to prevent from infinite loops.
 
-let routes;
-  if (isLoggedIn) {
+  //Managing token expiration
+  /* 
+    we have set 1hr to expire the token in the backend.
+    but still we have to manually expire the token which we have stored on the local storage.
+    Therfore, we have call the localstorage.removeItem in logout function.
+    yet we have to handle that logout to happen automatically using a useEffect.
+    2nd useEffect is here for that.
+  */
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.tokenExp) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.tokenExp));
+    }
+  }, [login]);
+  //Auto logout, when time expires
+  useEffect(() => {
+    if (token && tokenExpAuto) {
+      const timer = tokenExpAuto.getTime() - new Date().getTime();
+      logoutTime = setTimeout(logout, timer); //Executes the function, after waiting a specified number of milliseconds.
+    } else {
+      clearTimeout(logoutTime);
+    }
+  }, [token, logout, tokenExpAuto]);
+
+  let routes;
+  if (token) {
     routes = (
       <Switch>
         <Route path="/" exact>
           <Categories></Categories>
         </Route>
-        
+
         <Route path="/users" exact>
           <Users></Users>
         </Route>
@@ -47,9 +95,9 @@ let routes;
         <Route path="/new-product" exact>
           <NewProduct></NewProduct>
         </Route>
-        <Route exact path='/cat/:id'>
-					<ProductsByCat />
-				</Route>
+        <Route exact path="/cat/:id">
+          <ProductsByCat />
+        </Route>
 
         <Redirect to="/"></Redirect>
       </Switch>
@@ -66,10 +114,10 @@ let routes;
         <Route path="/new-category" exact>
           <NewCategory></NewCategory>
         </Route>
-        
-      <Route exact path='/cat/:id'>
-					<ProductsByCat />
-				</Route>
+
+        <Route exact path="/cat/:id">
+          <ProductsByCat />
+        </Route>
         <Route path="/auth">
           <Auth></Auth>
         </Route>
@@ -80,7 +128,8 @@ let routes;
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: isLoggedIn,
+        isLoggedIn: !!token,
+        token: token,
         userId: userId,
         login: login,
         logout: logout,
@@ -95,6 +144,5 @@ let routes;
     </AuthContext.Provider>
   );
 }
-
 
 export default App;
