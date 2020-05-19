add3f1fa405b2f078de3bc50bda9ec4ebad3781e
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-05-19 21:11:28 +0530

    Update on the product

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PRODUCT, GET_PRODUCTS, PRODUCT_ERROR, CLEAR_PRODUCTS } from './types';
+import { GET_PRODUCT, GET_PRODUCTS, PRODUCT_ERROR, CLEAR_PRODUCTS, CLEAR_PRODUCT } from './types';
 
 //GET PRODUCTS BY CATEGORY
 export const getProducts = (catId) => async (dispatch) => {
@@ -17,6 +17,24 @@ export const getProducts = (catId) => async (dispatch) => {
 		dispatch({
 			type: PRODUCT_ERROR,
 			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+export const getProduct = (prodId) => async (dispatch) => {
+	dispatch({
+		type: CLEAR_PRODUCT
+	});
+	try {
+		const res = await axios.get(`/api/product/${prodId}`);
+		dispatch({
+			type: GET_PRODUCT,
+			payload: res.data.product
+		});
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_ERROR,
+			payload: { msg: err.response.statusText }
 		});
 	}
 };
