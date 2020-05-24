473ca3e698059f6090fcd207326f4b7551bd741c
Dasun <97machan@gmail.com>
2020-05-24 13:30:32 +0530

    product view redifined

--- src/components/view-product/product.js
+++ src/components/view-product/product.js
@@ -11,6 +11,7 @@ import { AuthContext } from '../../Common/context/auth-context';
 import CustomButton from '../custom-button/custom-button.component';
 import { checkout } from '../../actions/order';
 import StarRating from '../star-rating/star-rating.component';
+import ViewReviews from '../view-reviews/view-review';
 import './product.styles.scss';
 
 const customStyles = {
@@ -68,184 +69,210 @@ const Product = ({
 
     return (
         <Fragment>
-            <div className='view-product'>
-                {productLoading ? (
-                    <div>Loading</div>
-                ) : (
-                    <Fragment>
-                        <div
-                            className='product-image'
-                            style={{
-                                backgroundImage: `url(${product.image})`
-                            }}
-                        />
-                        <div className='product-details'>
-                            <span className='title'>
-                                {product.category.name} - {product.name}{' '}
-                                {product.qty <= 30 ? (
-                                    <span className='hot-product'>
-                                        HOT ITEM
-                                    </span>
-                                ) : (
-                                    ''
-                                )}
-                            </span>
-
-                            <span className='review-count'>
-                                <StarRating
-                                    size={20}
-                                    disable={true}
-                                    rate={Math.round(all)}
-                                />
-                                {product.rating.length === 0
-                                    ? 'No reviews yet'
-                                    : `${all} rating with ${product.rating.length} reviews`}{' '}
-                            </span>
-                            <br />
-                            <span className='availableqty'>
-                                {product.qty <= 30 ? 'Only ' : ''}
-                                {product.qty} - Items available{' '}
-                            </span>
-                            <span className='price'>
-                                {product.discount ? (
-                                    <span>
-                                        {' '}
-                                        Discounted price{' '}
-                                        <span className='cutprice'>
-                                            LKR {product.price}
+            <div className='view-product-wrapper'>
+                <div className='view-product'>
+                    {productLoading ? (
+                        <div>Loading</div>
+                    ) : (
+                        <Fragment>
+                            <div
+                                className='product-image'
+                                style={{
+                                    backgroundImage: `url(${product.image})`
+                                }}
+                            />
+                            <div className='product-details'>
+                                <span className='title'>
+                                    {product.category.name} - {product.name}{' '}
+                                    {product.qty <= 30 ? (
+                                        <span className='hot-product'>
+                                            HOT ITEM
                                         </span>
-                                    </span>
-                                ) : (
-                                    ''
-                                )}{' '}
-                                <span
-                                    className={
-                                        product.discount ? 'newprice' : ''
-                                    }
-                                >
-                                    LKR{' '}
-                                    {product.discount
-                                        ? product.price - product.discount
-                                        : product.price}
+                                    ) : (
+                                        ''
+                                    )}
                                 </span>
-                            </span>
 
-                            <span className='size'>
-                                <span>Size</span>
-
-                                <span
-                                    className={`size-option ${
-                                        size === 'S' ? 'selected' : ''
-                                    }`}
-                                    onClick={() => changeSize('S')}
-                                >
-                                    S
-                                </span>
-                                <span
-                                    className={`size-option ${
-                                        size === 'M' ? 'selected' : ''
-                                    }`}
-                                    onClick={() => changeSize('M')}
-                                >
-                                    M
-                                </span>
-                                <span
-                                    className={`size-option ${
-                                        size === 'L' ? 'selected' : ''
-                                    }`}
-                                    onClick={() => changeSize('L')}
-                                >
-                                    L
-                                </span>
-                                <span
-                                    className={`size-option ${
-                                        size === 'XL' ? 'selected' : ''
-                                    }`}
-                                    onClick={() => changeSize('XL')}
-                                >
-                                    XL
-                                </span>
-                                <span
-                                    className={`size-option ${
-                                        size === 'XXL' ? 'selected' : ''
-                                    }`}
-                                    onClick={() => changeSize('XXL')}
-                                >
-                                    XXL
-                                </span>
-                            </span>
-
-                            <span className='qty'>
-                                Quantity{' '}
-                                <input
-                                    className={`qty-input ${
-                                        qty >= product.qty ? 'exceed' : ''
-                                    }`}
-                                    type='number'
-                                    name='qty'
-                                    value={qty}
-                                    onChange={(e) => onChange(e)}
-                                    min={1}
-                                    max={product.qty}
-                                />
-                            </span>
-                            <div className='wrapper'>
-                                <div className='addtocart-button'>
-                                    <CustomButton
-                                        onClick={() => {
-                                            auth.isLoggedIn
-                                                ? addItem(product, size, qty)
-                                                : (window.location.href =
-                                                      '/auth-user');
-                                        }}
-                                    >
-                                        Add to cart
-                                    </CustomButton>
-                                    <CustomButton
-                                        inverted
-                                        onClick={() => {
-                                            auth.isLoggedIn
-                                                ? setIsOpen(!modalIsOpen)
-                                                : (window.location.href =
-                                                      '/auth-user');
-                                        }}
-                                    >
-                                        Buy now
-                                    </CustomButton>
-                                    <WishListIcon
-                                        classname='wishlist-icon'
-                                        product={id}
+                                <span className='review-count'>
+                                    <StarRating
+                                        size={20}
+                                        disable={true}
+                                        rate={Math.round(all)}
                                     />
+                                    {product.rating.length === 0
+                                        ? 'No reviews yet'
+                                        : `${all} rating with ${product.rating.length} reviews`}{' '}
+                                </span>
+                                <br />
+                                <span className='availableqty'>
+                                    {product.qty <= 30 ? 'Only ' : ''}
+                                    {product.qty} - Items available{' '}
+                                </span>
+                                <span className='price'>
+                                    {product.discount ? (
+                                        <span>
+                                            {' '}
+                                            Discounted price{' '}
+                                            <span className='cutprice'>
+                                                LKR {product.price}
+                                            </span>
+                                        </span>
+                                    ) : (
+                                        ''
+                                    )}{' '}
+                                    <span
+                                        className={
+                                            product.discount ? 'newprice' : ''
+                                        }
+                                    >
+                                        LKR{' '}
+                                        {product.discount
+                                            ? product.price - product.discount
+                                            : product.price}
+                                    </span>
+                                </span>
+
+                                <span className='size'>
+                                    <span>Size</span>
+
+                                    <span
+                                        className={`size-option ${
+                                            size === 'S' ? 'selected' : ''
+                                        }`}
+                                        onClick={() => changeSize('S')}
+                                    >
+                                        S
+                                    </span>
+                                    <span
+                                        className={`size-option ${
+                                            size === 'M' ? 'selected' : ''
+                                        }`}
+                                        onClick={() => changeSize('M')}
+                                    >
+                                        M
+                                    </span>
+                                    <span
+                                        className={`size-option ${
+                                            size === 'L' ? 'selected' : ''
+                                        }`}
+                                        onClick={() => changeSize('L')}
+                                    >
+                                        L
+                                    </span>
+                                    <span
+                                        className={`size-option ${
+                                            size === 'XL' ? 'selected' : ''
+                                        }`}
+                                        onClick={() => changeSize('XL')}
+                                    >
+                                        XL
+                                    </span>
+                                    <span
+                                        className={`size-option ${
+                                            size === 'XXL' ? 'selected' : ''
+                                        }`}
+                                        onClick={() => changeSize('XXL')}
+                                    >
+                                        XXL
+                                    </span>
+                                </span>
+
+                                <span className='qty'>
+                                    Quantity{' '}
+                                    <input
+                                        className={`qty-input ${
+                                            qty >= product.qty ? 'exceed' : ''
+                                        }`}
+                                        type='number'
+                                        name='qty'
+                                        value={qty}
+                                        onChange={(e) => onChange(e)}
+                                        min={1}
+                                        max={product.qty}
+                                    />
+                                </span>
+                                <div className='wrapper'>
+                                    <div className='addtocart-button'>
+                                        <CustomButton
+                                            onClick={() => {
+                                                auth.isLoggedIn
+                                                    ? addItem(
+                                                          product,
+                                                          size,
+                                                          qty
+                                                      )
+                                                    : (window.location.href =
+                                                          '/auth-user');
+                                            }}
+                                        >
+                                            Add to cart
+                                        </CustomButton>
+                                        <CustomButton
+                                            inverted
+                                            onClick={() => {
+                                                auth.isLoggedIn
+                                                    ? setIsOpen(!modalIsOpen)
+                                                    : (window.location.href =
+                                                          '/auth-user');
+                                            }}
+                                        >
+                                            Buy now
+                                        </CustomButton>
+                                        <WishListIcon
+                                            classname='wishlist-icon'
+                                            product={id}
+                                        />
+                                    </div>
                                 </div>
                             </div>
+                        </Fragment>
+                    )}
+                </div>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={() => setIsOpen(false)}
+                    style={customStyles}
+                    ariaHideApp={false}
+                >
+                    <div
+                        className='modal-close-button'
+                        onClick={() => setIsOpen(false)}
+                    >
+                        &#9932;
+                    </div>
+
+                    <Link
+                        to='/checkout'
+                        onClick={() => checkout(product, qty, size, total)}
+                    >
+                        <CustomButton>
+                            <FaCcVisa className='facard' size={30} /> CARD
+                        </CustomButton>
+                    </Link>
+
+                    <CustomButton>CASH ON DELIVEY</CustomButton>
+                </Modal>
+
+                {/* {!productLoading && product.rating.length ? (
+                    <ViewReviews ratings={product.rating} />
+                ) : (
+                    <div className='no-reviews'>
+                        <span>No reviews yet</span>
+                    </div>
+                )} */}
+
+                {!productLoading ? (
+                    product.rating.length ? (
+                        <ViewReviews ratings={product.rating} />
+                    ) : (
+                        <div className='no-reviews'>
+                            <span>No reviews yet</span>
                         </div>
-                    </Fragment>
+                    )
+                ) : (
+                    ''
                 )}
             </div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setIsOpen(false)}
-                style={customStyles}
-                ariaHideApp={false}
-            >
-                <div
-                    className='modal-close-button'
-                    onClick={() => setIsOpen(false)}
-                >
-                    &#9932;
-                </div>
-
-                <Link
-                    to='/checkout'
-                    onClick={() => checkout(product, qty, size, total)}
-                >
-                    <CustomButton>
-                        <FaCcVisa className='facard' size={30} /> CARD
-                    </CustomButton>
-                </Link>
-
-                <CustomButton>CASH ON DELIVEY</CustomButton>
-            </Modal>
         </Fragment>
     );
 };
