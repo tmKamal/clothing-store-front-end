39b9a02dc3d69550bab9b0a65e1c39d16c16bfa5
Dasun <97machan@gmail.com>
2020-05-25 01:28:57 +0530

    images refined

--- src/category/pages/categories.jsx
+++ src/category/pages/categories.jsx
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useHttpClient } from "../../Common/custom-hooks/http-hook";
+import React, { useEffect, useState } from 'react';
+import { useHttpClient } from '../../Common/custom-hooks/http-hook';
 
-import CategoryList from "../components/category-list/category-list"
-import ErrorModal from "../../Common/components/UIElements/model/error-model";
-import LoadingSpinner from "../../Common/components/UIElements/loading-spinner/loading-spinner";
+import CategoryList from '../components/category-list/category-list';
+import ErrorModal from '../../Common/components/UIElements/model/error-model';
+import LoadingSpinner from '../../Common/components/UIElements/loading-spinner/loading-spinner';
 
 const Categories = () => {
-  const {isLoading,error,sendRequest,errorPopupCloser}=useHttpClient();
-  const [loadedCategories, setLoadedCategories] = useState();
+    const { isLoading, error, sendRequest, errorPopupCloser } = useHttpClient();
+    const [loadedCategories, setLoadedCategories] = useState();
 
-  useEffect(() => {// we have use useEffect hook to stop the infinite loop. otherwise fetch will rerender to all the changes. 
-    const fetchCategories = async () => {// this method is only to use the async code. we can't use async directly on the useEffect hook. useEffect is not good for promisses.
-      
-      try {
-        const responseData = await sendRequest("https://quiet-hollows-79620.herokuapp.com/api/categories/");
-        
-        
-        setLoadedCategories(responseData.categories);//this is the key of the JSON response - view the backend code for more.
-      } catch (err) {
-        
-      }
-      
-    };
-    fetchCategories();
-  }, [sendRequest]);
-  
-  return (
-    <React.Fragment>
-      <ErrorModal error={error} onClear={errorPopupCloser}></ErrorModal>
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner></LoadingSpinner>
-        </div>
-      )}
-      {loadedCategories && !isLoading && <CategoryList items={loadedCategories} />}
-    </React.Fragment>
-  );
+    useEffect(() => {
+        // we have use useEffect hook to stop the infinite loop. otherwise fetch will rerender to all the changes.
+        const fetchCategories = async () => {
+            // this method is only to use the async code. we can't use async directly on the useEffect hook. useEffect is not good for promisses.
+
+            try {
+                const responseData = await sendRequest(
+                    'https://quiet-hollows-79620.herokuapp.com/api/categories/'
+                );
+
+                setLoadedCategories(responseData.categories); //this is the key of the JSON response - view the backend code for more.
+            } catch (err) {}
+        };
+        fetchCategories();
+    }, [sendRequest]);
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorPopupCloser}></ErrorModal>
+            {isLoading && (
+                <div className='center'>
+                    <LoadingSpinner></LoadingSpinner>
+                </div>
+            )}
+            {loadedCategories && !isLoading && (
+                <CategoryList items={loadedCategories} />
+            )}
+        </React.Fragment>
+    );
 };
 
 export default Categories;
