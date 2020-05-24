c10cb427006596f373f4836e5d116ee6d5fafbf0
Thanura09 <thanura.marapana@gmail.com>
2020-05-24 13:15:13 +0530

    rating system frontend

--- src/components/view-reviews/review.jsx
+++ src/components/view-reviews/review.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React from 'react';
+import StarRating from '../star-rating/star-rating.component';
+import './review.style.scss';
 
-export const Review = ({ userName, rate, comment }) => {
-  return <div></div>;
+const Review = ({ rating }) => {
+    return (
+        <div className='col-md-6 rating-component'>
+            <div className='row'>
+                <div
+                    className='col-4 user-image'
+                    style={{
+                        backgroundImage: `url(${rating.userId.image})`
+                    }}
+                ></div>
+                <div className='col-8'>
+                    {' '}
+                    <span className='row'>
+                        <span className='col-12 username'>
+                            {rating.userId.name}{' '}
+                            <span className='rating'>
+                                - {rating.rate} star rating
+                            </span>
+                        </span>
+                        <span className='col-12 rating'>
+                            <StarRating rate={rating.rate} disable={true} />
+                        </span>
+                    </span>
+                    <span className='row comment'>{rating.comment}</span>
+                </div>
+            </div>
+        </div>
+    );
 };
+
+export default Review;
